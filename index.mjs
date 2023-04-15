be2220c47a240a113ad82a5228b6c11d6da1a987
Raphael Menges <unrealer2@yahoo.de>
2023-04-15 11:46:26 +0200

    Fix crawler.

--- index.mjs
+++ index.mjs
@@ -1,31 +1,37 @@
 import Crawler from "crawler";
 
-const c = new Crawler({});
+const c = new Crawler();
 let obselete = [];
+const base = "https://www.semanux.com";
 
 // Based on: https://stackoverflow.com/a/50164565
 const crawlAllUrls = (url) => {
   console.log(url);
   c.queue({
     uri: url,
-    callback: (err, res, done) => {
-      let $ = res.$;
-      let urls = $("a");
-      Object.keys(urls).forEach((item) => {
-        if (urls[item].type === "tag") {
-          let href = urls[item].attribs.href;
-          if (href !== undefined && !obselete.includes(href)) {
-            href = href.trim();
-            if(href.startsWith("/")) {
-              obselete.push(href);
-              href.startsWith('http') ? crawlAllUrls(href) : crawlAllUrls(`${url}${href}`);
+    callback: (_err, res, done) => {
+      try {
+        let $ = res.$;
+        let urls = $("a");
+        Object.keys(urls).forEach((item) => {
+          if (urls[item].type === "tag") {
+            let href = urls[item].attribs.href;
+            if (href !== undefined) {
+              href = href.trim();
+              if(href.startsWith("/")) {
+                href = `${base}${href}`;
+                if(!obselete.includes(href)) {
+                  obselete.push(href);
+                  crawlAllUrls(href);
+                }
+              }
             }
           }
-        }
-      });
-      done();
+        });
+        done();
+      } catch (_e) {}
     }
   });
 }
 
-crawlAllUrls('https://www.semanux.com');
+crawlAllUrls(base);
